0b68072e12624ae275faa5b92dc9e8bdc885b5c1
ArjunChoudhary2 <arjun219207choudhary@gmail.com>
2026-05-24 11:44:02 +0530

    cors fix

--- EmailSender.Api/Program.cs
+++ EmailSender.Api/Program.cs
@@ -52,7 +52,8 @@ builder.Services.AddCors(options =>
                 "http://localhost:5173",
                 "http://localhost:5174",
                 "http://127.0.0.1:5173",
-                "http://127.0.0.1:5174"
+                "http://127.0.0.1:5174",
+                "https://aesthetic-valkyrie-44f6be.netlify.app/"
             ]
             : configuredOrigins.Split(',', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
 
